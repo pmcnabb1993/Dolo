779e71b6135f5b665cdbb60e9138696f830299ad
brandonwc5 <brandonwc5@yahoo.com>
2018-02-22 19:17:23 -0600

    working server

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-//require
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app =  express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+var routes = require('./controllers/Router.js');
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+app.post('');
+app.listen(PORT);
