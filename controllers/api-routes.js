91b440cd49081e1252de56c012bbce0b1855ca5f
GitDarren <darren321@gmail.com>
2018-02-24 10:37:43 -0600

    updated api-routes and html-routes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -27,8 +27,30 @@ app.post("/api/login", passport.authentication("local"), function(req, res) {
         }).catch(function(err)  {
             console.log(err);
             res.json(err);
-        })
-    })
+            // res.status(422).json(err.errors[0].message);
+        });
+    });
 
+    //Route for logging user out
+    app.get("/logout", function(req, res)   {
+        req.logout();
+        res.redirect("/");
+    });
 
-}
+    //Route for getting some data about our user to be used client side
+    app.get("/api/user_data", function(req, res)    {
+        if(!req.user)   {
+            //The user is not logged in, send back an empty object
+            res.json({});
+        }
+        else {
+            //Otherwise send back the user's email and id
+            //Sending back a password, even a hashed password, isn't a good idea
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
+    });
+
+};
