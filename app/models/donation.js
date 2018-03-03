a8ee77495adcd96024861ff0b85705d23b16086d
GitDarren <darren321@gmail.com>
2018-03-03 00:58:57 -0600

    4th attempt to solve donation posting error: - Unhandled rejection SequelizeForeignKeyConstraintError: Cannot add or update a child row: a foreign key constraint fails (`p1lrjjvf40i7cbv5`.`Donations`, CONSTRAINT `Donations_ibfk_2` FOREIGN KEY (`item_categoryID`) REFERENCES `Item_Categories` (`id`))

--- app/models/donation.js
+++ app/models/donation.js
@@ -12,17 +12,17 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.TEXT,
       allowNull: false
     },
-    uid: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Users',
-        key: 'id'
-      }
-    },
     item_categoryID: {
       type: DataTypes.INTEGER,
       references: {
         model: 'Item_Categories',
+        key: 'id'
+      }
+    },
+    uid: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
         key: 'id'
       }
     },
